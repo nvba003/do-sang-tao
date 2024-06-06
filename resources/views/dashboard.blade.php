111263241d4497e1f6e8a2b18588d6e03b8bb06f
nvba003 <nvba003@gmail.com>
2024-06-06 08:38:57 +0700

    update dashboard UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <a href="{{ route('fetch.products') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Cập nhật sản phẩm</a>
                 </div>
             </div>
         </div>
     </div>
 
-    <a href="{{ route('fetch.products') }}" class="btn btn-primary">Fetch and Store Products</a>
     @if (session('success'))
         <script>
             window.onload = function() {
